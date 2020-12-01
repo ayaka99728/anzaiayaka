8746d0a53400c066d92c09018295b886ac547938
ayaka99728 <zm18500@josai.ac.jp>
2020-12-01 02:19:58 +0000

    1201

--- ex6-5.js
+++ ex6-5.js
@@ -1,6 +1,7 @@
-function sayhello65() {var who = 'world';alert('Hello, ' + who65 + '!');}
+var who65 = 'world';function sayhello65() {alert('Hello, ' + who65 + '!');}
 function taro65() {who65 = '太郎';}
 function hanako65() {who65 = '花子';}
 
-function taro66() {who = '太郎';alert('太郎が来た');}
-function hanako66() {who = '花子';alert('花子が来た');}
+var who66 = 'world';function sayhello66() {alert('Hello, ' + who66 + '!');}
+function taro66() {who66 = '太郎';alert('太郎が来た');}
+function hanako66() {who66 = '花子';alert('花子が来た');}
